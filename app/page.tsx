a76cc9845250e4efb29bc85793e4bdc58db1068a
Steve-B17 <steve.joel.cbe@gmail.com>
2024-07-13 21:38:50 +0530

    Day 1 13/7/2024

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import PatientForm from "@/components/ui/forms/PatientForm";
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl ">Home Page</h1>
-    </div>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+        <Image 
+          src="/assets/icons/logo-full.svg"
+          alt="patient"
+          width={1000}
+          height={1000}
+          className="mb-12 h-10 w-fit"/>
+          <PatientForm/>
+          <div className="text-14-regular mt-20 flex justify-between"><p>© 2024 Carepulse</p></div>
+        </div>
+      </section>   
+      </div>
   );
 }
